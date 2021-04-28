1b456dd58ea3ab61d6a61387125b6b1bd26f49db
Gerardo Cetzal <gerardo.cetzal@lighthousetech.io>
2021-04-28 09:55:51 -0500

    Initial Commit

---
+++ rest-api/rest_apiApp.swift
@@ -0,0 +1,17 @@
+//
+//  rest_apiApp.swift
+//  rest-api
+//
+//  Created by Gerardo Cetzal on 28/04/21.
+//
+
+import SwiftUI
+
+@main
+struct rest_apiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
